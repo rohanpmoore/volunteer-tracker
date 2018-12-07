f2535327e99619a9d73b4eba557d8848bfc3c352
Rohan Moore <rohanpmoore@gmail.com>
2018-12-07 12:53:03 -0800

    == spec success

--- lib/project.rb
+++ lib/project.rb
@@ -12,4 +12,23 @@ class Project
     result = DB.exec("INSERT INTO projects (name) VALUES ('#{@title}') RETURNING ID")
     @id = result.first["id"].to_i
   end
+
+  def self.all_basic(projects)
+    output_projects = []
+    projects.each do |project|
+      id = project["id"]
+      title = project["name"]
+      output_projects.push(Project.new({:title => title, :id => id}))
+    end
+    output_projects
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects")
+    Project.all_basic(returned_projects)
+  end
+
+  def ==(other_instance)
+    @title == other_instance.title
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -21,13 +21,13 @@ describe Project do
     end
   end
 
-  # describe '#==' do
-  #   it 'is the same project if two projects have the same title' do
-  #     project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     project2 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     expect(project1 == project2).to eq true
-  #   end
-  # end
+  describe '#==' do
+    it 'is the same project if two projects have the same title' do
+      project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project2 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      expect(project1 == project2).to eq true
+    end
+  end
   #
   # context '.all' do
   #   it 'is empty to start' do
@@ -42,7 +42,7 @@ describe Project do
   #     expect(Project.all).to eq [project1, project2]
   #   end
   # end
-  #
+
   # describe '#save' do
   #   it 'saves a project to the database' do
   #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
@@ -50,7 +50,7 @@ describe Project do
   #     expect(Project.all).to eq [project]
   #   end
   # end
-  #
+
   # describe '.find' do
   #   it 'returns a project by id' do
   #     project1 = Project.new({:title => 'Teaching Kids to Code', :id => nil})
