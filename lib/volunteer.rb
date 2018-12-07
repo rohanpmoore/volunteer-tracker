4890530d5b47f0d278b363a98c0d97bf4a9ad1c6
Rohan Moore <rohanpmoore@gmail.com>
2018-12-07 12:59:50 -0800

    pass name spec volunteer

---
+++ lib/volunteer.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Volunteer
+  attr_reader :name, :hours, :project_id, :id
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @hours = attributes[:hours]
+    @project_id = attributes[:project_id]
+    @id = attributes[:id]
+    if(!@hours)
+      @hours = 0
+    end
+  end
+end
