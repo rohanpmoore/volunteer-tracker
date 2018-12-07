f9e613ec308e934dd080bcb2a0a22c75a0658b1d
Rohan Moore <rohanpmoore@gmail.com>
2018-12-07 13:15:53 -0800

    pass delete spec project

--- lib/project.rb
+++ lib/project.rb
@@ -45,4 +45,8 @@ class Project
     @title = attributes[:title]
     DB.exec("UPDATE projects SET name = '#{@title}' WHERE id = #{@id}")
   end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id}")
+  end
 end
